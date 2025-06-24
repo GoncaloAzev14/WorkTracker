51b4c67fe99cf97981a13a4b3bd802f660ecd67b
GoncaloAzev14 <1211560@isep.ipp.pt>
2025-06-24 22:13:07 +0100

    Initial Commit

---
+++ WorkTracker/WorkTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTrackerApp.swift
+//  WorkTracker
+//
+//  Created by Gonçalo Azevedo on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkTrackerTests/WorkTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTrackerTests.swift
+//  WorkTrackerTests
+//
+//  Created by Gonçalo Azevedo on 24/06/2025.
+//
+
+import Testing
+
+struct WorkTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
