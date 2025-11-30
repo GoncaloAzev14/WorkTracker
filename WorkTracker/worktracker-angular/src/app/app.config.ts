10a9757dc47bfe415ce7c3cae2711668b90eeb77
GoncaloAzev14 <1211560@isep.ipp.pt>
2025-11-30 23:07:58 +0000

    feat: implement service worker and update application configuration for PWA support

--- WorkTracker/worktracker-angular/src/app/app.config.ts
+++ WorkTracker/worktracker-angular/src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt-PT';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+// Registar os dados de localização para pt-PT
+registerLocaleData(localePt);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    { provide: LOCALE_ID, useValue: 'pt-PT' },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
 };
